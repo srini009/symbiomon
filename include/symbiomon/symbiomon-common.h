31e310f547c394107c36b1ed974d5d03cc47b31c
Srinivasan Ramesh <ramesh.srinivasan09@gmail.com>
2021-01-15 07:35:28 -0500

    Add parathesis around while and remove warning

--- include/symbiomon/symbiomon-common.h
+++ include/symbiomon/symbiomon-common.h
@@ -71,8 +71,9 @@ hash(char *str)
     uint64_t hash = 5381;
     uint64_t c;
 
-    while (c = *str++)
+    while (c = *str++) {
         hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
 
     return hash;
 }
