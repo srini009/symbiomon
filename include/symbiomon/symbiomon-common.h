6ec08ec51279e567742c0ee6153443de04cb4d84
Srinivasan Ramesh <ramesh.srinivasan09@gmail.com>
2021-01-15 07:42:30 -0500

    Hopefully this actually fixes the warning

--- include/symbiomon/symbiomon-common.h
+++ include/symbiomon/symbiomon-common.h
@@ -71,7 +71,7 @@ hash(char *str)
     uint64_t hash = 5381;
     uint64_t c;
 
-    while (c = *str++) {
+    while ((c = *str++)) {
         hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
     }
 
