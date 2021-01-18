db1cbea688a9a7d591b72c983e653219b76c2aa9
Srinivasan Ramesh <ramesh.srinivasan09@gmail.com>
2021-01-18 06:53:00 -0500

    Add more printfs

--- src/provider.c
+++ src/provider.c
@@ -193,7 +193,7 @@ static void symbiomon_metric_fetch_ult(hg_handle_t h)
         goto finish;
     }
 
-    fprintf(stderr, "Inside server provider ULT for metric fetch: I get here with metric_id, and provider_id: %lu, %u\n", in.metric_id, provider->provider_id);
+    fprintf(stderr, "Inside server provider ULT for metric fetch: I get here with metric_id, and provider_id, and provider head metric: %lu, %u, %lu\n", in.metric_id, provider->provider_id, provider->metrics->id);
     symbiomon_metric_id_t requested_id = in.metric_id;
     symbiomon_metric* metric = find_metric(provider, &(requested_id));
     if(!metric) {
