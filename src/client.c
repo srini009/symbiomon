5909efba2a532d5978b855c71f41c786155952a2
Srinivasan Ramesh <ramesh.srinivasan09@gmail.com>
2021-02-26 15:55:03 -0500

    Add sample id field

--- src/client.c
+++ src/client.c
@@ -195,7 +195,7 @@ symbiomon_return_t symbiomon_metric_dump_raw_data(symbiomon_metric_t m, const ch
     FILE *fp = fopen(filename, "w");
     int i;
     for(i = 0; i < m->buffer_index; i++)
-        fprintf(fp, "%.9lf, %.9lf\n", m->buffer[i].val, m->buffer[i].time);
+        fprintf(fp, "%.9lf, %.9lf, %lu\n", m->buffer[i].val, m->buffer[i].time, m->buffer[i].sample_id);
     fclose(fp);
 }
 
