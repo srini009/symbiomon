72d14d715cf7a23e736a0323ff6131006c308a14
Srinivasan Ramesh <ramesh.srinivasan09@gmail.com>
2021-04-12 18:11:44 -0400

    Add some more changes

--- src/provider.c
+++ src/provider.c
@@ -424,6 +424,7 @@ symbiomon_return_t symbiomon_provider_metric_reduce(symbiomon_metric_t m, symbio
 	case SYMBIOMON_REDUCTION_OP_MAX: {
 	    int i=0;
             double max = -9999999999.0;
+            fprintf(stderr, "Before: max is: %lf\n", max);
 	    for(i=0; i < current_index; i++) {
                 max = (m->buffer[i].val > max ? m->buffer[i].val:max);
                 fprintf(stderr, "At the client: name: %s_MAX, agg_id: %d, checking against: %lf, max is: %lf, current_index is: %d\n", m->stringify, agg_id, m->buffer[i].val, max, current_index);
@@ -432,6 +433,7 @@ symbiomon_return_t symbiomon_provider_metric_reduce(symbiomon_metric_t m, symbio
 	    strcpy(key, m->stringify);
 	    strcat(key, "_");
 	    strcat(key, "_MAX");
+            fprintf(stderr, "After: max is: %lf\n", max);
             fprintf(stderr, "At the client before sending: name: %s, agg_id: %d, and max: %lf, current_index: %d\n", key, agg_id, max, current_index);
              
 	    ret = sdskv_put(provider->aggphs[agg_id], provider->aggdbids[agg_id], (const void *)key, strlen(key), &max, sizeof(double));
