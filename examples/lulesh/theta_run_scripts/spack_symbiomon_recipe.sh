944657ba11028801f767f5ae69a7a78e3d456791
Srinivasan Ramesh <ramesh.srinivasan09@gmail.com>
2022-07-26 19:07:26 -0400

    Remove spack install recipe

--- examples/lulesh/theta_run_scripts/spack_symbiomon_recipe.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-spack install mochi-symbiomon@develop+aggregator+bedrock+reducer ^mochi-reducer@develop+aggregator ^mochi-sdskv@develop-test+aggrservice ^libfabric fabrics=gni %gcc@7.3.0
-spack install mochi-sdskv@develop-test+aggrservice+symbiomon ^libfabric fabrics=gni %gcc@7.3.0
-spack install mochi-reducer@develop+aggregator+symbiomon ^mochi-sdskv@develop-test+aggrservice+symbiomon ^libfabric fabrics=gni %gcc@7.3.0
-#spack install py-mochi-symbiomon ^mochi-symbiomon@develop+aggregator+bedrock+reducer ^libfabric fabrics=verbs,rxm ^python@2.7.5+ucs4
-
